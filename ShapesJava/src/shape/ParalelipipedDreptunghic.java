748415cf4bce0bccf31e430f1e91dedf5e934eee
IancuPetru <84980308+IancuPetru@users.noreply.github.com>
2021-06-16 18:05:19 +0300

    Update ParalelipipedDreptunghic.java

--- ShapesJava/src/shape/ParalelipipedDreptunghic.java
+++ ShapesJava/src/shape/ParalelipipedDreptunghic.java
@@ -2,16 +2,9 @@ package shape;
 
 import base.ShapeBase;
 
-public class ParalelipipedDreptunghic extends ShapeBase {
-//int result;
-	public ParalelipipedDreptunghic(String name, int width, int height, int height1,String formula) {
-		super(name, width, height,height1, formula);
-	
-		// TODO Auto-generated constructor stub
-	}
+public class RectangularCuboid extends ShapeBase {
 
-public int  calculateArea() {
-		
-		return 2*(width*height+width*height1+height*height1);
-}
+	public RectangularCuboid(String name,String formula) {
+		super(name="rectagular cuboid",formula="2*(width*length+width*height+length*height)");
+	}
 }
