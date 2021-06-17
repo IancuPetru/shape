105978c3773fffeb699889c537a179088182cd3f
IancuPetru <84980308+IancuPetru@users.noreply.github.com>
2021-06-17 17:03:13 +0300

    Revert "Update Triangle.java"

--- ShapesJava/src/shape/Triangle.java
+++ ShapesJava/src/shape/Triangle.java
@@ -4,7 +4,13 @@ import base.ShapeBase;
 
 public class Triangle  extends ShapeBase{
 
-	public Triangle(String name,String formula) {
-		super(name="triangle",formula="width*height/2");
-		}
+	public Triangle(String name, int width, int height,String formula) {
+		super(name, width, height, formula);
+		// TODO Auto-generated constructor stub
+	}
+
+	public int calculateArea() {
+		
+		return width*height/2;
+	}
 }
