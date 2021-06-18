dfa7dfbcda1a577bc6aa485ba5f7a3a9a1946e93
IancuPetru <84980308+IancuPetru@users.noreply.github.com>
2021-06-18 14:48:48 +0300

    Update RectangularCuboid.java

--- ShapesJava/src/shape/RectangularCuboid.java
+++ ShapesJava/src/shape/RectangularCuboid.java
@@ -4,7 +4,7 @@ import base.ShapeBase;
 
 public class RectangularCuboid extends ShapeBase {
 
-	public RectangularCuboid(String name,String formula) {
-		super(name="rectagular cuboid",formula="2*(width*length+width*height+length*height)");
+	public RectangularCuboid() {
+		super("rectagular cuboid","2*(width*length+width*height+length*height)");
 	}
 }
