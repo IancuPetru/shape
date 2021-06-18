8ae20e7f27d48b7b526f5adf5f3c341ace69c3e9
IancuPetru <84980308+IancuPetru@users.noreply.github.com>
2021-06-18 14:51:23 +0300

    Update Test.java

--- ShapesJava/src/test/Test.java
+++ ShapesJava/src/test/Test.java
@@ -11,9 +11,9 @@ public class Test {
 		System.out.println("the area for the shape named:"+" "+base.get.Name() "+ "is calculated using this:"+" "+base.getFormula());
 	public static void main(String[] args) {
 		ShapeBase [] shapes=new ShapeBase[3];
-		shapes[0]=new Rectangle("","");
-		shapes[1]=new Triangle("","");
-		shapes[2]=neW RectangularCuboid("","");
+		shapes[0]=new Rectangle();
+		shapes[1]=new Triangle();
+		shapes[2]=neW RectangularCuboid();
 	
 		for(int i=0;i<shapes.length;i++) {
 			displayAreaFormula(shapes[i]);}
