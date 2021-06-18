3798dd1c8555fa7e52f8bc800f07c4cd9a783eaa
IancuPetru <84980308+IancuPetru@users.noreply.github.com>
2021-06-18 14:47:16 +0300

    Update Rectangle.java

--- ShapesJava/src/shape/Rectangle.java
+++ ShapesJava/src/shape/Rectangle.java
@@ -4,8 +4,8 @@ import base.ShapeBase;
 
 public class Rectangle extends ShapeBase {
 
-	public Rectangle(String name,String formula) {
-		super(name="rectangle",formula="width*height");
+	public Rectangle() {
+		super("rectangle","width*height");
 		
 	}
 }
